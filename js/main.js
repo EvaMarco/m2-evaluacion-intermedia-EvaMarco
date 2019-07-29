8cbe9b79ff1074790eb3fcb4f69db30c9c31c47a
Migara <evamarcod@gmail.com>
2019-07-29 09:51:03 +0200

    Add name modal

--- js/main.js
+++ js/main.js
@@ -6,17 +6,19 @@ const hint = document.querySelector('.js__hint');
 const triesOut = document.querySelector('.js__tries');
 const historic = document.querySelector('.js__historic');
 const btnReset = document.querySelector('.js__btn-reset');
-
+const btnClose = document.querySelector('.js__btnClose');
+const inputName = document.querySelector('.js__inputName');
+const modal = document.querySelector('.js__modal');
+let name = '';
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
 }
 
-
-let myRandomNumber = getRandomNumber(100);
-
-console.log(`Mi número aleatorio es ${myRandomNumber}`);
-
-let tries = parseInt(triesOut.innerHTML);
+function close(event){
+    name = inputName.value;
+    modal.classList.add('hidden');
+    return name
+}
 
 function reset(event){
     myRandomNumber = getRandomNumber(100);
@@ -40,15 +42,21 @@ function getHint(event){
         triesOut.innerHTML = tries;
     }
     else{
-        hint.innerHTML = `¡Has ganado, Campeona!`
+        hint.innerHTML = `¡Has ganado, ${name}!`
         tries += 1;
         triesOut.innerHTML = tries;
-        historic.innerHTML += `<li>El numero a descubrir fue ${myRandomNumber} y tardaste ${tries} intentos en adivinarlo. </li>`
+        historic.innerHTML += `<li>El numero a descubrir fue ${myRandomNumber} y tardaste ${tries} intentos en adivinarlo. Muy bien ${name}. </li>`
         myRandomNumber = getRandomNumber(100);
         console.log(`Mi número aleatorio es ${myRandomNumber}`);
         tries = 0;
     }
 }
 
+let myRandomNumber = getRandomNumber(100);
+let tries = parseInt(triesOut.innerHTML);
+
+console.log(`Mi número aleatorio es ${myRandomNumber}`);
+
 btn.addEventListener('click', getHint);
 btnReset.addEventListener('click', reset);
+btnClose.addEventListener('click', close);
