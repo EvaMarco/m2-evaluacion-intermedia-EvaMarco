b5c0903921c1ea6edd70b56a596b4fcde853111b
Migara <evamarcod@gmail.com>
2019-08-04 15:12:00 +0200

    Add parseInt to input value, and add label to input in html

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ console.log(`Mi número aleatorio es ${myRandomNumber}`);
 let tries = parseInt(triesOut.innerHTML);
 
 function getHint(event){
-    const userNum = input.value;
+    const userNum = parseInt(input.value);
     if (userNum > myRandomNumber){
         hint.innerHTML = `Su número es mayor que el número buscado`
         tries += 1;
